e2500f288e25468ef85465e900137c6b79b2e18d
fahadkhanhussain2-rgb <fahad.khan.hussain2@gmail.com>
2026-09-05 10:59:03 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,23 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
 
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
